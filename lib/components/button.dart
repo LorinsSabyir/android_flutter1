d27d1bfdd312e0d5e9b037299f165f48bf0791f6
LorinsSabyir <lorins.sabyir@gmail.com>
2024-12-09 20:42:32 +0800

    sa

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,37 +1,33 @@
 import 'package:flutter/material.dart';
 
-class Button extends StatelessWidget {
-  final Function()? onTap;
-  final String label;
+class Button extends StatelessWidget{
 
-   Button({super.key, required this.onTap, this.label = 'Button'});
+  final Function()? onTap;
+  const Button({super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: SizedBox(
-        width: 150,  // Fixed width
-        height: 50,  // Fixed height
-        child: Container(
-          padding: EdgeInsets.all(10),
-          margin: EdgeInsets.symmetric(horizontal: 10),
-          decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: Center(
-            child: Text(
-              label,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
+  return GestureDetector(
+    onTap: onTap,
+    child: Container(
+      padding: EdgeInsets.all(25),
+      margin: EdgeInsets.symmetric(horizontal: 25),
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(10),
+        ),
+
+      child: Center(
+        child: Text(
+          'LOG IN',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            ),
             ),
           ),
-        ),
-      ),
-    );
+    ),
+  );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
-import 'test/cipher/atbash.dart'; // Adjust the import path based on your project structure
-import 'test/cipher/caesar.dart';
-import 'test/cipher/vigenere.dart';
+import 'loginpages/login.dart';
+
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Atbash Cipher',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Vigenere(), // Load the Atbash screen
+    return  MaterialApp(
+     debugShowCheckedModeBanner: false,
+     home: Login(),
     );
   }
 }
