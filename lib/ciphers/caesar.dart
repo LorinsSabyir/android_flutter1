b347a997383e3890638856bd595148d526d5e066
Lei Lorenz Xavier <lorins.sabyir@gmail.com>
2024-12-13 01:22:05 +0800

    payts

--- lib/ciphers/caesar.dart
+++ lib/ciphers/caesar.dart
@@ -20,12 +20,10 @@ class _CaesarState extends State<Caesar> {
     return text.split('').map((char) {
       if (char.codeUnitAt(0) >= 65 && char.codeUnitAt(0) <= 90) {
         // Uppercase letters
-        return String.fromCharCode(
-            (char.codeUnitAt(0) - 65 + shift) % 26 + 65);
+        return String.fromCharCode((char.codeUnitAt(0) - 65 + shift) % 26 + 65);
       } else if (char.codeUnitAt(0) >= 97 && char.codeUnitAt(0) <= 122) {
         // Lowercase letters
-        return String.fromCharCode(
-            (char.codeUnitAt(0) - 97 + shift) % 26 + 97);
+        return String.fromCharCode((char.codeUnitAt(0) - 97 + shift) % 26 + 97);
       } else {
         // Non-alphabetic characters
         return char;
@@ -64,90 +62,83 @@ class _CaesarState extends State<Caesar> {
         centerTitle: true,
         backgroundColor: Color(0xFF00646C),
       ),
-    
-        body:Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/bg1.jpeg'),
-              fit: BoxFit.cover,
-            )          
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/bg1.jpeg'),
+            fit: BoxFit.cover,
           ),
-      child: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.lock,
-                  size: 100,
-                  color: Colors.grey[700],
-                ),
-                SizedBox(height: 20),
-                Text(
-                  'This is Caesar Cipher',
-                  style: TextStyle(
-                    color: Colors.grey[800],
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+        ),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.lock,
+                    size: 100,
+                    color: Colors.grey[700],
                   ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 30),
-                MyTextField(
-                  controller: inputController,
-                  hintText: 'Enter Message Here',
-                  obscureText: false,
-                  maxLines: 3,
-                  
-                ),
-                SizedBox(height: 20),
-                MyTextField(
-                  controller: shiftController,
-                  hintText: 'Enter Shift Value',
-                  obscureText: false,
-                  keyboardType: TextInputType.number,
-                  inputFormatter:[
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  //keyboardType: TextInputType.number,
-                ),
-                SizedBox(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Button(
-                      onTap: encryptText,
-                      label: 'Encrypt',
-                      width: 100,
-                      color: Color(0xFF1B4A56),
+                  SizedBox(height: 20),
+                  Text(
+                    'This is Caesar Cipher',
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Button(
-                      onTap: decryptText,
-                      label: 'Decrypt',
-                      width: 100,
-                          color: Color(0xFF1B4A56),
-                    ),
-                    Button(
-                      onTap: clearText,
-                      label: 'Clear',
-                      width: 100,
-                  color: Color(0xFFAC4040),
-                  
-                  
-
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Result(resultText: resultText),
-              ],
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 30),
+                  MyTextField(
+                    controller: inputController,
+                    hintText: 'Enter Message Here',
+                    obscureText: false,
+                    maxLines: 3,
+                  ),
+                  SizedBox(height: 20),
+                  MyTextField(
+                    controller: shiftController,
+                    hintText: 'Enter Shift Value',
+                    obscureText: false,
+                    keyboardType: TextInputType.number,
+                    inputFormatter: [FilteringTextInputFormatter.digitsOnly],
+                    //keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Button(
+                        onTap: encryptText,
+                        label: 'Encrypt',
+                        width: 100,
+                        color: Color(0xFF1B4A56),
+                      ),
+                      Button(
+                        onTap: decryptText,
+                        label: 'Decrypt',
+                        width: 100,
+                        color: Color(0xFF1B4A56),
+                      ),
+                      Button(
+                        onTap: clearText,
+                        label: 'Clear',
+                        width: 100,
+                        color: Color(0xFFAC4040),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Result(resultText: resultText),
+                ],
+              ),
             ),
           ),
         ),
-
-      ),),
+      ),
     );
   }
 }
